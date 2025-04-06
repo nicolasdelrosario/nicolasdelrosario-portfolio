b18db862a4df10cea12397855e9eba0c9d4dc51c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-06 18:26:03 -0500

    chore: :art: Reorder imports in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
